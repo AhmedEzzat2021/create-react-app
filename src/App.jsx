85a6174f819b2c697791d5d647fabbb4a9876675
Ahmed Ezzat <ahmed.ezzat20232023@gmail.com>
2023-08-25 11:45:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import "./App.css";
+import Home from "./Pages/Home";
+
+function App() {
+   return (
+      <div className="App">
+         <Box>
+            <Home />
+         </Box>
+      </div>
+   );
+}
+
+export default App;
